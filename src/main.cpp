c09c71268faf0d0835caa0ff6d7b1b998ebf4e1b
Peter Matthew Bond <p.bond@cern.ch>
2019-05-22 15:13:09 +0000

    Merge branch 'relocate-examples' into 'manage-examples'
    Relocate examples
    
    See merge request alialfred/FREDServer!11

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
+/*
+Uncomment commented lines and recompile to use MAPI example
+*/
+
 #include <stdio.h>
 #include "Fred/fred.h"
 
-#include "Mapi/mapiexample.h"
+//#include "Mapi/mapiexample.h"
 
 int main()
 {
     pair<string, string> config = Fred::readConfigFile();
     Fred fred(config.first, config.second, "./sections");
 
-    MapiExample mapiExample;
-    fred.registerMapiObject("FRED1/MAPI_EXAMPLE/LOOP0/TEST", &mapiExample);
+    //MapiExample mapiExample;
+    //fred.registerMapiObject("FRED1/MAPI_EXAMPLE/LOOP0/TEST", &mapiExample);
 
     fred.Start();
 
